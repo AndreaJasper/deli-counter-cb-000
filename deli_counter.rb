68a98098fe4aa0c70e78175f1758ce6a858dffdc
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-11 18:50:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,11 @@ def take_a_number (deli, name)
 end
 
 def now_serving
-
+  if
+    deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serverin #{deli.first}"
+    deli.shift
+  end
 end
